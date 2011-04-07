b6006d74ca7711c71a2a879de6e6862bdeb0e8da
Luke Amdor <luke.amdor@t8webware.com>
2011-04-07 15:19:45 -0500

    fixing syntax error. (we really need to get to be able to compile for a plugin)

--- src/main/scala/utility.scala
+++ src/main/scala/utility.scala
@@ -30,7 +30,7 @@ trait CiTask extends BasicScalaProject { scct: ScctProject =>
   lazy val ci = task {
     ciActions.foldLeft(None: Option[String]) { (result, task) => result orElse act(task.name) }
   } describedAs "Runs ci tasks"
-  lazy val ccq = task { List(compile, console-quick).foldLeft(None:Option[String]){ (result,task) => result orElse act(task.name) }
+  lazy val ccq = consoleQuickAction dependsOn compile
 }
 
 trait SnapshotOrRelease extends BasicScalaProject {
