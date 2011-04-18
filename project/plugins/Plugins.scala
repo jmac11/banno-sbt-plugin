25aa5e6459eb26e491ae797b231bc5507c82b60a
Luke Amdor <luke.amdor@t8webware.com>
2011-04-18 14:18:41 -0500

    Have to use the databinder repo

--- project/plugins/Plugins.scala
+++ project/plugins/Plugins.scala
@@ -1,6 +1,6 @@
 import sbt._
 
 class Plugins(info: ProjectInfo) extends PluginDefinition(info) {
-  val bannoExternalRepo   = "Banno External Repo" at "http://10.3.0.26:8081/nexus/content/groups/external/"
+  val databinder_repo = Resolver.url("Databinder Repository", new java.net.URL("http://databinder.net/repo"))(Resolver.ivyStylePatterns)
   val scriptedDep = "org.scala-tools.sbt" % "scripted" % "0.7.4"
 }
