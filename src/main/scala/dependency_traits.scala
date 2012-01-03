f68b61c6c80d48d098416977e805837ae754c1e3
Joseph Winder <joseph.winder@t8webware.com>
2012-01-03 13:58:07 -0600

    New version of joda needs joda convert dependency.

--- src/main/scala/dependency_traits.scala
+++ src/main/scala/dependency_traits.scala
@@ -2,6 +2,7 @@ import sbt._
 import reaktor.scct.ScctProject
 
 trait BannoCommonDeps extends BasicScalaProject {
+  lazy val jodaConvert = "org.joda" % "joda-convert" % "1.1"
   lazy val jodaTime = "joda-time" % "joda-time" % "2.0"
   lazy val scalajCollection = buildScalaVersion match {
     case "2.7.7" => "org.scalaj" % "scalaj-collection_2.8.0" % "1.0"
