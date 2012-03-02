c0ac661c204f20b40bd93a2c8b61bc844e56bf60
Luke Amdor <luke.amdor@t8webware.com>
2012-03-01 21:43:25 -0600

    not overriding settings so it doesn't get auto-added

--- src/main/scala/BannoSettings.scala
+++ src/main/scala/BannoSettings.scala
@@ -2,8 +2,8 @@ package com.banno
 import sbt._
 import Keys._
 
-object BannoSettings extends Plugin {
-  override val settings =
+object BannoSettings {
+  val settings =
     Seq(organization := "com.banno",
         scalaVersion := "2.9.1",
         version := "1.0-SNAPSHOT") ++
