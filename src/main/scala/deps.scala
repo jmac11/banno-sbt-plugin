8a9aeced25aa3c69ac363548b53c3fd30f96f0a3
Luke Amdor <luke.amdor@t8webware.com>
2012-03-01 21:53:04 -0600

    handling akka1 and akka2 group change

--- src/main/scala/deps.scala
+++ src/main/scala/deps.scala
@@ -14,10 +14,11 @@ object BannoCommonDeps {
 object Akka {
   val version = SettingKey[String]("akka-version")
 
-  def akkaModule(module: String, v: String) = "se.scalablesolutions.akka" % ("akka-" + module) % v
+  def akkaModule(module: String, v: String) =
+    (if (v.startsWith("1")) "se.scalablesolutions.akka" else "com.typesafe.akka") % ("akka-" + module) % v
 
   val settings: Seq[Project.Setting[_]] = Seq(
-    version := "1.2",
+    version := "1.3.1",
     libraryDependencies <++= (version) { v =>
       Seq(akkaModule("actor", v), akkaModule("remote", v))
     }
