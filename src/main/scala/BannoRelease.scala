e09d49965f52c42d4abc5e8d9ec0d6e21f0bd166
Luke Amdor <luke.amdor@t8webware.com>
2012-05-10 16:50:58 -0500

    release if changed takes code changes into effect

--- src/main/scala/BannoRelease.scala
+++ src/main/scala/BannoRelease.scala
@@ -3,20 +3,25 @@ import sbt._
 import Keys._
 import sbtrelease._
 import sbtrelease.ReleaseKeys._
+import ReleaseStateTransformations._
 
 object BannoRelease {
+
+  val ignorableCodeChangePaths = SettingKey[Seq[String]]("ignorable-code-change-paths")
+
   val settings = Release.releaseSettings ++ Seq(
+    ignorableCodeChangePaths := Seq(bannoDependenciesFileName, "version.sbt"),
+
+    commands += releaseIfChanged,
+
     tagName <<= (version in ThisBuild)(identity),
-    releaseVersion <<= (organization, name, scalaVersion) { (g, a, s) => { _ =>
-      val latestReleasedVersion = Nexus.latestReleasedVersionFor(g, a + "_" + s)
-      latestReleasedVersion.flatMap(v => Version(v).map(_.bumpBugfix.string)).getOrElse(versionFormatError)
-    }},
-    nextVersion := { ver => Version(ver).map(_.copy(bugfix = None)).map(_.asSnapshot.string).getOrElse(versionFormatError) },
+    releaseVersion <<= (organization, name, scalaVersion)(getLastVersionFromNexusAndIncrement),
+    nextVersion := removeMicroAndAddSnapshot,
+
     releaseProcess <<= thisProjectRef apply { ref =>
-      import ReleaseStateTransformations._
-      Seq[ReleasePart](
+      Seq(
         initialGitChecks,
-        // checkSnapshotDependencies,
+        checkSnapshotDependencies,
         inquireVersions,
         updateReleaseBannoDeps,
         setReleaseVersion,
@@ -30,16 +35,47 @@ object BannoRelease {
         push
       )
     }
+
   )
 
-  val updateReleaseBannoDeps: ReleasePart = { st =>
+  def removeMicroAndAddSnapshot(ver: String) = { Version(ver).map(_.copy(bugfix = None)).map(_.asSnapshot.string).getOrElse(versionFormatError) }
+
+  def getLastVersionFromNexusAndIncrement(org: String, name: String, scalaVers: String): (String => String) = { _ =>
+      val latestReleasedVersion = Nexus.latestReleasedVersionFor(org, name + "_" + scalaVers)
+      latestReleasedVersion.flatMap(v => Version(v).map(_.bumpBugfix.string)).getOrElse(versionFormatError)
+    }
+
+  val releaseIfChanged: Command = Command.command("release-if-changed") {
+    st: State =>
+    if (bannoDependenciesHaveBeenUpdated(st) || codeChangedSinceLastRelease(st)) {
+      val extracted = Project.extract(st)
+      val process = extracted.get(releaseProcess)
+      val startState = st.put(useDefaults, true)
+      Function.chain(process)(startState)
+    } else {
+      st.log.info("No changes so no release")
+      st
+    }
+  }
+
+  def bannoDependenciesHaveBeenUpdated(st: State): Boolean = latestReleasedVersionsForBannoDeps(st).exists {
+    case (dep, latestVersion) =>
+      val extract = Project.extract(st)
+      val key = SettingKey[String]("%s-released-version".format(dep.name))
+      extract.get(key) != latestVersion
+  }
+
+  def latestReleasedVersionsForBannoDeps(st: State): Seq[Pair[ModuleID, String]] = {
     val extract = Project.extract(st)
-    st.log.info("Updating banno dependencies to latest releases")
-    val bannoDeps = extract.get(bannoDependencies)
-    val withLatestRelease = bannoDeps.map { dep =>
+    extract.get(bannoDependencies).map { dep =>
       val depArtifactId = dep.name + "_" + extract.get(scalaVersion)
       dep -> Nexus.latestReleasedVersionFor(dep.organization, depArtifactId).getOrElse(sys.error("No release found for %s".format(depArtifactId)))
     }
+  }
+
+  def updateReleaseBannoDeps(st: State): State = {
+    st.log.info("Updating banno dependencies to latest releases")
+    val withLatestRelease = latestReleasedVersionsForBannoDeps(st)
 
     // write to file
     val newSettingsContent =  withLatestRelease.map {
@@ -62,7 +98,7 @@ object BannoRelease {
     ReleaseStateTransformations.reapply(newReleaseVersionSettings, st)
   }
 
-  val commitReleaseBannoDepsVersions: ReleasePart = { st =>
+  def commitReleaseBannoDepsVersions(st: State): State = {
     val modified = Process("git" :: "status" :: "--porcelain" :: "--" :: bannoDependenciesFileName :: Nil) !! st.log
     if (!modified.isEmpty) {
       Git.add(bannoDependenciesFileName) !! st.log
@@ -71,7 +107,21 @@ object BannoRelease {
     st
   }
 
-  val push: ReleasePart = { st =>
+
+  def codeChangedSinceLastRelease(st: State): Boolean = {
+    val extract = Project.extract(st)
+    val maybeLastRelease = Nexus.latestReleasedVersionFor(extract.get(organization),
+                                                     extract.get(name) + "_" + extract.get(scalaVersion))
+    maybeLastRelease.map { lastRelease =>
+      val ignorablePaths = extract.get(ignorableCodeChangePaths)
+      val diffRevisions = "%s..HEAD".format(lastRelease)
+      val diff = (Process("git" :: "diff" :: diffRevisions :: "--name-only" :: Nil) !!).split("\n").toSet
+      val realDiff = diff -- ignorablePaths
+      !realDiff.isEmpty
+    } getOrElse true
+  }
+
+  def push(st: State) = {
     Git.pushTags !! st.log
     Git.pushCurrentBranch !! st.log
     st
