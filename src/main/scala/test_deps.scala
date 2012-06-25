1abcdbefea25cc0cc6e22b601d23313561aa75a9
Luke Amdor <luke.amdor@t8webware.com>
2012-06-25 08:03:05 -0500

    updating dependencies versions

--- src/main/scala/test_deps.scala
+++ src/main/scala/test_deps.scala
@@ -6,7 +6,7 @@ object Specs2 {
   val version = SettingKey[String]("specs2-version")
 
   val settings: Seq[Project.Setting[_]] = Seq(
-    version := "1.8.1",
+    version := "1.11",
     libraryDependencies <+= (version) { v =>
       "org.specs2" %% "specs2" % v % "test"
     }
@@ -18,7 +18,7 @@ object ScalaTest {
   val version = SettingKey[String]("scalatest-version")
 
   val settings: Seq[Project.Setting[_]] = Seq(
-    version := "1.7.1",
+    version := "1.8",
     libraryDependencies <+= (version) { v =>
       "org.scalatest" %% "scalatest" % v % "test"
     }
