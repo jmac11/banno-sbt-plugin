a4533095b188940234ee9a35d997500e403cffcf
Luke Amdor <luke.amdor@gmail.com>
2013-03-04 21:18:02 -0600

    adding clear-local-banno-artifacts task

--- src/main/scala/BannoNexus.scala
+++ src/main/scala/BannoNexus.scala
@@ -6,7 +6,8 @@ import aether.Aether
 object BannoNexus {
   val bannoSnapshots = "Banno Snapshots Repo" at "http://nexus.banno.com/nexus/content/repositories/snapshots"
   val bannoReleases = "Banno Releases Repo" at "http://nexus.banno.com/nexus/content/repositories/releases"
-  val NEXUS_UPDATE_METADATA_JOB_ID = "6"
+
+  val clearLocalBannoArtifacts = TaskKey[Unit]("clear-local-banno-artifacts", "Clear locally published and downloaded artifacts from ivy cache")
 
   val settings: Seq[Project.Setting[_]] = Seq(
     resolvers := Seq(
@@ -24,6 +25,13 @@ object BannoNexus {
         Some(bannoReleases)
       }
     },
-    credentials += Credentials(Path.userHome / ".ivy2" / ".banno_credentials")
+    credentials += Credentials(Path.userHome / ".ivy2" / ".banno_credentials"),
+    clearLocalBannoArtifacts := { 
+      import IO._
+      import Path._
+      delete(userHome / ".ivy2" / "cache" / "com.banno")
+      delete(userHome / ".ivy2" / "local" / "com.banno")
+      delete(userHome / ".ivy2" / "cache" / "scala_2.9.2" / "sbt_0.12" / "com.banno")
+    }
   ) ++ Aether.aetherPublishSettings
 }
