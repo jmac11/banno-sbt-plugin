bff7033c08f2ff2fb3a16cfd4dd17f7bfb6debc5
jwinder <joseph.winder@t8webware.com>
2013-03-22 09:25:45 -0500

    Bumping specs2 and scalacheck versions.

--- src/main/scala/test_deps.scala
+++ src/main/scala/test_deps.scala
@@ -6,7 +6,7 @@ object Specs2 {
   val version = SettingKey[String]("specs2-version")
 
   val settings: Seq[Project.Setting[_]] = Seq(
-    version := "1.12.3",
+    version := "1.14",
     libraryDependencies <+= (version) { v =>
       "org.specs2" %% "specs2" % v % "test"
     }
@@ -17,7 +17,7 @@ object Scalacheck {
   val version = SettingKey[String]("scalacheck-version")
 
   val settings: Seq[Project.Setting[_]] = Seq(
-    version := "1.9",
+    version := "1.10.0",
     libraryDependencies <+= (version) { v =>
       "org.scalacheck" %% "scalacheck" % v % "test"
     }
