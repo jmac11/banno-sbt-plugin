ce9c14b4b77543172ff439619790363d8d18494f
Luke Amdor <luke.amdor@banno.com>
2013-06-14 08:47:32 -0500

    we don't care about the assembly jar name

--- src/main/scala/FatJar.scala
+++ src/main/scala/FatJar.scala
@@ -7,7 +7,6 @@ import Plugin.AssemblyKeys._
 
 object FatJar {
   val settings: Seq[Project.Setting[_]] = Plugin.assemblySettings ++ Seq(
-    jarName in assembly <<= (name, scalaVersion, version) map { _ + "_" + _ + "-" + _ + "-assembly.jar" },
     packagedArtifacts <<= (packagedArtifacts, assembly, name) map ( (arts, file, n) => arts.updated(Artifact(n, "assembly"), file) ),
     artifacts <+= (name) { n => Artifact(n, "assembly") },
     test in assembly := { }
