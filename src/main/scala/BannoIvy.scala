7a64b9f55b7634fa700269bb73bb44caf3e59bf0
Luke Amdor <luke.amdor@banno.com>
2013-06-13 21:56:08 -0500

    organizing default ivy excludes

--- src/main/scala/BannoIvy.scala
+++ src/main/scala/BannoIvy.scala
@@ -6,11 +6,16 @@ import scala.xml._
 object BannoIvy {
   val excludes =
     Seq(
+      // all other crappy logging systems
+      "commons-logging" -> "commons-logging",
+      "log4j" -> "log4j",
       "org.slf4j" -> "slf4j-log4j12",
       "org.slf4j" -> "slf4j-simple",
+
+      // why eclipse???
       "org.eclipse.jetty.orbit" -> "javax.servlet",
-      "org.mortbay.jetty" -> "servlet-api",
-      "org.mortbay.jetty" -> "servlet-api-2.5",
+
+      // the beheamouth of unnecessary hbase deps
       "org.jruby" -> "jruby-complete",
       "com.sun.jersey" -> "jersey-core",
       "com.sun.jersey" -> "jersey-server",
@@ -20,14 +25,13 @@ object BannoIvy {
       "tomcat" -> "jasper-runtime",
       "org.mortbay.jetty" -> "jetty",
       "org.mortbay.jetty" -> "jetty-util",
+      "org.mortbay.jetty" -> "servlet-api",
+      "org.mortbay.jetty" -> "servlet-api-2.5",
       "org.mortbay.jetty" -> "jsp-2.1",
       "org.mortbay.jetty" -> "jsp-api-2.1",
-      "stax" -> "stax-api",
-      "org.apache.avro" -> "avro-ipc",
       "org.apache.hadoop" -> "avro",
-      "org.apache.thrift" -> "thrift",
-      "commons-logging" -> "commons-logging",
-      "log4j" -> "log4j"
+      "org.apache.avro" -> "avro-ipc",
+      "org.apache.thrift" -> "thrift"
     )
 
   def defaultExcludes(additionalExcludes: Pair[String,String]*) = {
