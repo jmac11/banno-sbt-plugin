87bd7a4b680c271781c3ae2d5aae9818d48f5571
jwinder <joseph.winder@t8webware.com>
2013-08-09 08:44:41 -0500

    Adding test-deps for HBaseTestingUtility.

--- src/main/scala/test_deps.scala
+++ src/main/scala/test_deps.scala
@@ -39,3 +39,28 @@ object ScalaTest {
   )
 
 }
+
+object HBaseTestingUtility {
+  val hadoopVersion = SettingKey[String]("hadoop-version")
+  val hbaseVersion = SettingKey[String]("hbase-version")
+
+  // The "-tests" dependencies are really test classifier artifacts. However, ivy and sbt don't really get along with that.
+  // i.e. "org.apache.hbase" % "hbase" % v % "test" classifier "tests",
+  // So we had to manually upload them to nexus with a different artifact id i.e. hadoop-common became hadoop-common-tests
+
+  val settings: Seq[Project.Setting[_]] = Seq(
+    hadoopVersion := "2.0.0-cdh4.3.0",
+    hbaseVersion := "0.94.6-cdh4.3.0",
+    libraryDependencies <++= (hadoopVersion) { v =>
+      Seq("org.apache.hadoop" % "hadoop-mapreduce-client-jobclient" % v % "test",
+          "org.apache.hadoop" % "hadoop-hdfs" % v % "test",
+          "org.apache.hadoop" % "hadoop-common-tests" % v % "test",
+          "org.apache.hadoop" % "hadoop-hdfs-tests" % v % "test",
+          "org.apache.hadoop" % "hadoop-mapreduce-client-jobclient-tests" % v % "test")
+    },
+    libraryDependencies <++= (hbaseVersion) { v =>
+      Seq("org.apache.hbase" % "hbase-tests" % v % "test",
+          "log4j" % "log4j" % "1.2.17" % "test")
+    }
+  )
+}
