a54eccff4d43690301e55d08fd9acc66d7bf86a1
Luke Amdor <luke.amdor@banno.com>
2013-10-02 07:23:56 -0500

    updating specs2 versions

--- src/main/scala/test_deps.scala
+++ src/main/scala/test_deps.scala
@@ -7,8 +7,8 @@ object Specs2 {
 
   val settings: Seq[Project.Setting[_]] = Seq(
     version <<= (scalaVersion) apply {
-      case sv if sv.startsWith("2.9") => "1.12.3"
-      case _ => "2.1"
+      case sv if sv.startsWith("2.9") => "1.12.4.1"
+      case _ => "2.2.2"
     },
     libraryDependencies <+= (version) { v =>
       "org.specs2" %% "specs2" % v % "test"
