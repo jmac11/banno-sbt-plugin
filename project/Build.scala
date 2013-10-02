afaeac72d700f6994d6ac1d209dc5d708c0b919f
Luke Amdor <luke.amdor@banno.com>
2013-10-02 07:26:29 -0500

    bumping version

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object MyBuild extends Build {
 
   val myBuildSettings = Seq(sbtPlugin := true,
                             name := "banno-sbt-plugin",
-                            version := "1.2.14",
+                            version := "1.2.15",
                             organization := "com.banno",
 
                             publishArtifact in (Compile, packageSrc) := false,
