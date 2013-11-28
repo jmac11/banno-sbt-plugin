c28e4900f2d32eb1dcf20fa8387d940ac3a32cf7
Adam Shannon <adam@ashannon.us>
2013-11-28 15:26:13 -0600

    Adding some additional compiler warnings around code that can use non-explicit issues.
    Source of flags and meanings: 
    https://github.com/scala/scala/blob/2.10.x/src/compiler/scala/tools/nsc/settings/Warnings.scala#L47

--- src/main/scala/BannoCompile.scala
+++ src/main/scala/BannoCompile.scala
@@ -8,7 +8,9 @@ object BannoCompile {
       case sv if sv.startsWith("2.9") =>
         Seq("-deprecation", "-unchecked")
       case _ =>
-        Seq("-deprecation", "-feature", "-language:postfixOps", "-language:implicitConversions", "-unchecked")
+        Seq("-deprecation", "-feature", "-language:postfixOps", "-language:implicitConversions", 
+            "-Ywarn-adapted-args", "-Ywarn-dead-code", "-Ywarn-numeric-widen", "-Ywarn-inaccessible", 
+            "-Ywarn-infer-any", "-unchecked")
     },
     scalacOptions <++= (scalaVersion) map { sv =>
       if (sv.startsWith("2.9")) Seq("-Ydependent-method-types") else Nil
