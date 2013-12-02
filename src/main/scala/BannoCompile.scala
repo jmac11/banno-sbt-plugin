214decc66872a4c2d70cbda42e263b5af589aca9
Adam Shannon <adam.shannon@banno.com>
2013-12-02 13:08:29 -0600

    -Ywarn-infer-any seems to have gone away in 2.10.x

--- src/main/scala/BannoCompile.scala
+++ src/main/scala/BannoCompile.scala
@@ -8,9 +8,8 @@ object BannoCompile {
       case sv if sv.startsWith("2.9") =>
         Seq("-deprecation", "-unchecked")
       case _ =>
-        Seq("-deprecation", "-feature", "-language:postfixOps", "-language:implicitConversions", 
-            "-Ywarn-adapted-args", "-Ywarn-dead-code", "-Ywarn-numeric-widen", "-Ywarn-inaccessible", 
-            "-Ywarn-infer-any", "-unchecked")
+        Seq("-deprecation", "-feature", "-language:postfixOps", "-language:implicitConversions",
+            "-Ywarn-adapted-args", "-Ywarn-dead-code", "-Ywarn-numeric-widen", "-Ywarn-inaccessible", "-unchecked")
     },
     scalacOptions <++= (scalaVersion) map { sv =>
       if (sv.startsWith("2.9")) Seq("-Ydependent-method-types") else Nil
