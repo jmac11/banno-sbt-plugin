12764d8e25ef039f40c10077bde707d58c989023
Trent Johnson <trent.johnson@banno.com>
2013-12-27 14:05:06 -0600

    Bumping the version number

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object MyBuild extends Build {
 
   val myBuildSettings = Seq(sbtPlugin := true,
                             name := "banno-sbt-plugin",
-                            version := "1.3.1",
+                            version := "1.3.2",
                             organization := "com.banno",
                             shellPrompt <<= (name) { (name) => _ => name + " > " },
 
