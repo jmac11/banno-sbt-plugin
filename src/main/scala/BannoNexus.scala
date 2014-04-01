7fbe3f5cd707a7f3ecc46411766a68f13d21acce
jj <jj@banno.com>
2014-04-01 17:36:29 -0500

    Aether was replacing the publish command with its own and replacing any previous setup attached to the original command. This changes Aether to depend on the original before it replaces it. Ran into the issue with doing dist publishes for Play 2.2.0.

--- src/main/scala/BannoNexus.scala
+++ src/main/scala/BannoNexus.scala
@@ -37,5 +37,7 @@ object BannoNexus {
       delete(userHome / ".ivy2" / "cache" / "scala_2.10.1" / "sbt_0.12" / "com.banno")
       delete(userHome / ".ivy2" / "cache" / "scala_2.10" / "sbt_0.13" / "com.banno")
     }
-  ) ++ Aether.aetherPublishSettings
+  ) ++ fixedAetherPublishSettings
+
+  lazy val fixedAetherPublishSettings = Aether.aetherSettings ++ Seq(publish <<= Aether.deploy.dependsOn(publish))
 }
