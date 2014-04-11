a7814ac5159a049676abe8d500f5feca8d089f7c
Luke Amdor <luke.amdor@banno.com>
2014-04-11 13:48:35 -0500

    adding nicer bannoProject helpers for build.sbt

--- src/main/scala/BannoSubProject.scala
+++
@@ -1,9 +0,0 @@
-package com.banno
-import sbt._
-import Keys._
-
-case class BannoSubProject(pid: String, root: Project,
-                           defaultSettings: Seq[Setting[_]] = BannoSettings.settings ++ Seq(publish := {}, publishLocal := {})) {
-
-  lazy val proj = project.dependsOn(root % "compile->test").settings(name := pid).settings(defaultSettings: _*)
-}
