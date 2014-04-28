2c2aa6ed77d39d06ec946028ae7aa6f797352673
Luke Amdor <luke.amdor@banno.com>
2014-04-28 14:54:01 -0500

    using correct version of metrics-scala based on scala version

--- src/main/scala/deps.scala
+++ src/main/scala/deps.scala
@@ -106,7 +106,7 @@ object Metrics {
   val settings: Seq[Setting[_]] = Seq(
     version := "3.0.2",
     libraryDependencies <++= (version, Akka.version) { (v, av) =>
-      val msv = if (av startsWith "2.1") "3.0.3_a2.1.0" else "3.0.4" //https://github.com/erikvanoosten/metrics-scala#available-versions
+      val msv = s"3.0.3_a${av.take(3)}" //https://github.com/erikvanoosten/metrics-scala#available-versions
       Seq(
         "com.codahale.metrics" % "metrics-core"     % v,
         "com.codahale.metrics" % "metrics-graphite" % v,
