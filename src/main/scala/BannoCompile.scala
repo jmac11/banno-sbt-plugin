992d800538c416085a039da4d106e2abaef25ec1
Luke Amdor <luke.amdor@gmail.com>
2014-05-22 15:53:12 -0500

    removing older scala 2.9 compile options

--- src/main/scala/BannoCompile.scala
+++ src/main/scala/BannoCompile.scala
@@ -4,23 +4,9 @@ import Keys._
 
 object BannoCompile {
   val settings = Seq(
-    scalacOptions <++= (scalaVersion) map {
-      case sv if sv.startsWith("2.9") =>
-        Seq("-deprecation", "-unchecked")
-      case _ =>
-        Seq("-deprecation", "-feature", "-language:postfixOps",
-            "-language:implicitConversions", "-language:higherKinds", "-language:existentials", "-language:postfixOps",
-            "-Ywarn-adapted-args", "-Ywarn-dead-code", "-Ywarn-numeric-widen", "-Ywarn-inaccessible", "-unchecked")
-    },
-    scalacOptions <++= (scalaVersion) map { sv =>
-      if (sv.startsWith("2.9")) Seq("-Ydependent-method-types") else Nil
-    },
-    scalacOptions <++= (version) map { v =>
-      if (v.endsWith("SNAPSHOT")) Nil else Seq("-optimize")
-    },
-    scalacOptions in Test <++= (scalaVersion) map {
-      case sv if sv.startsWith("2.9") => Nil
-      case _                          => Seq("-language:reflectiveCalls")
-    }
+    scalacOptions := Seq("-deprecation", "-feature", "-language:postfixOps",
+                         "-language:implicitConversions", "-language:higherKinds", "-language:existentials", "-language:postfixOps",
+                         "-Ywarn-adapted-args", "-Ywarn-dead-code", "-Ywarn-numeric-widen", "-Ywarn-inaccessible", "-unchecked"),
+    scalacOptions in Test += "-language:reflectiveCalls"
   )
 }
