eef969c4b13629ac6da321372b1d6a198a57e635
Eric Heikes <eric.heikes@banno.com>
2014-07-15 17:25:02 -0500

    Prevent bower from erroring out when there are version conflicts.

--- project/Build.scala
+++ project/Build.scala
@@ -11,7 +11,7 @@ object MyBuild extends Build {
 
   val myBuildSettings = Seq(sbtPlugin := true,
                             name := "banno-sbt-plugin",
-                            version := "1.3.16",
+                            version := "1.3.17",
                             organization := "com.banno",
                             shellPrompt <<= (name) { (name) => _ => name + " > " },
 
