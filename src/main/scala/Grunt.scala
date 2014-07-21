d21c24c9d9869ac3cc25643b2f4eb2ffb26ee4ee
Eric Heikes <eric.heikes@banno.com>
2014-07-21 10:40:29 -0500

    Fix npm/bower/grunt dependency hierarchy.

--- src/main/scala/Grunt.scala
+++ src/main/scala/Grunt.scala
@@ -3,14 +3,43 @@ import sbt._
 import Keys._
 
 object Grunt {
+  val npm = TaskKey[Unit]("npm")
+
+  val bowerClientDirectory = SettingKey[File]("bower-client-directory")
+  val bowerOutputDirectory = SettingKey[File]("bower-output-directory")
+  val bowerExecutable = SettingKey[String]("bower-executable")
+  val bower = TaskKey[Seq[File]]("bower")
+  val bowerWatchSources = TaskKey[Seq[File]]("bower-watch-sources")
+
   val gruntClientDirectory = SettingKey[File]("grunt-client-directory")
   val gruntOutputDirectory = SettingKey[File]("grunt-output-directory")
   val gruntExecutable = SettingKey[String]("grunt-executable")
   val grunt = TaskKey[Seq[File]]("grunt")
-  val npm = TaskKey[Unit]("npm")
   val gruntWatchSources = TaskKey[Seq[File]]("grunt-watch-sources")
 
   val settings = Seq(
+    // NPM
+    npm <<= (baseDirectory) map (Process("npm" :: "install" :: Nil, _) !),
+
+    // Bower
+    bowerExecutable := "bower",
+    bowerClientDirectory <<= baseDirectory apply (bd => bd / "src/main/client"),
+    bowerOutputDirectory <<= (resourceManaged in Compile) apply (out => out / "public"),
+
+    bowerWatchSources <<= bowerClientDirectory map (d => (d ***).get),
+    watchSources <<= (watchSources, bowerWatchSources) map ( (ws, bws) => ws ++ bws),
+
+    bowerExecutable <<= baseDirectory apply (bd => (bd / "node_modules/bower/bin/bower").toString),
+
+    bower <<= (bowerExecutable, baseDirectory, bowerOutputDirectory) map { (be, bd, out) =>
+      val exitCode = (Process(be :: "install" :: "--allow-root" :: "--force-latest" :: Nil, bd, "OUTPUT_DIR" -> out.toString) !)
+      if (exitCode != 0) sys.error(s"Bower nonzero exit code: ${exitCode}. Aborting!")
+      (out ***).get
+    },
+    bower <<= bower.dependsOn(npm),
+    resourceGenerators in Compile <+= bower.task,
+
+    // Grunt
     gruntExecutable := "grunt",
     gruntClientDirectory <<= baseDirectory apply (bd => bd / "src/main/client"),
     gruntOutputDirectory <<= (resourceManaged in Compile) apply (out => out / "public"),
@@ -19,7 +48,7 @@ object Grunt {
     watchSources <<= (watchSources, gruntWatchSources) map ( (ws, gws) => ws ++ gws),
 
     npm <<= (baseDirectory) map (Process("npm" :: "install" :: Nil, _) !),
-    gruntExecutable <<= baseDirectory apply (bd => (bd / "node_modules/grunt-cli/bin/grunt").toString), // since grunt-cli is a npm dev dpe
+    gruntExecutable <<= baseDirectory apply (bd => (bd / "node_modules/grunt-cli/bin/grunt").toString),
 
     grunt <<= (gruntExecutable, baseDirectory, gruntOutputDirectory) map { (ge, bd, out) =>
       val exitCode = (Process(ge, bd, "OUTPUT_DIR" -> out.toString) !)
@@ -27,6 +56,7 @@ object Grunt {
       (out ***).get
     },
     grunt <<= grunt.dependsOn(npm),
+    grunt <<= grunt.dependsOn(bower),
     resourceGenerators in Compile <+= grunt.task
   )
 }
