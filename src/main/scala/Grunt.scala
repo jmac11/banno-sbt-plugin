f192889f876282774b60ca5b1e77f2b77cb02128
Luke Amdor <luke.amdor@banno.com>
2014-10-01 16:30:33 -0500

    cleaning up Grunt.settings and bower stuff

--- src/main/scala/Grunt.scala
+++ src/main/scala/Grunt.scala
@@ -3,55 +3,56 @@ import sbt._
 import Keys._
 
 object Grunt {
+  val clientSrcDirectory = SettingKey[File]("client-src-directory")
+
   val npm = TaskKey[Unit]("npm")
 
-  val bowerClientDirectory = SettingKey[File]("bower-client-directory")
-  val bowerOutputDirectory = SettingKey[File]("bower-output-directory")
-  val bowerExecutable = SettingKey[String]("bower-executable")
-  val bower = TaskKey[Seq[File]]("bower")
-  val bowerWatchSources = TaskKey[Seq[File]]("bower-watch-sources")
+  val bowerExecutable = SettingKey[File]("bower-executable")
+  val bowerJsonFile = SettingKey[File]("bower-json-file")
+  val bowerComponentsDirectory=  SettingKey[File]("bower-components-directory")
+  val bower = TaskKey[Unit]("bower")
 
-  val gruntClientDirectory = SettingKey[File]("grunt-client-directory")
-  val gruntOutputDirectory = SettingKey[File]("grunt-output-directory")
-  val gruntExecutable = SettingKey[String]("grunt-executable")
-  val grunt = TaskKey[Seq[File]]("grunt")
   val gruntWatchSources = TaskKey[Seq[File]]("grunt-watch-sources")
+  val gruntExecutable = SettingKey[File]("grunt-executable")
+  val gruntOutputDirectory = SettingKey[File]("output-directory")
+  val gruntDefaultTask = SettingKey[String]("grunt-default-task")
+  val grunt = TaskKey[Seq[File]]("grunt")
 
   val settings = Seq(
+    clientSrcDirectory := baseDirectory.value / "src/main/client",
+
     // NPM
-    npm <<= (baseDirectory) map (Process("npm" :: "install" :: "--registry" :: "http://npm.banno-internal.com" :: Nil, _) !),
+    npm :=  {
+      val exitCode = (Process("npm" :: "install" :: "--registry" :: "http://npm.banno-internal.com" :: Nil, baseDirectory.value) !)
+      if (exitCode != 0) sys.error("'npm install' failed!")
+    },
 
-    // Bower
-    bowerExecutable := "node_modules/bower/bin/bower",
-    bowerClientDirectory <<= baseDirectory apply (bd => bd / "src/main/client"),
-    bowerOutputDirectory <<= (resourceManaged in Compile) apply (out => out / "public"),
-
-    bowerWatchSources <<= bowerClientDirectory map (d => (d ***).get),
-    watchSources <<= (watchSources, bowerWatchSources) map ( (ws, bws) => ws ++ bws),
-
-    bower <<= (bowerExecutable, baseDirectory, bowerOutputDirectory) map { (be, bd, out) =>
-      val exitCode = (Process(be :: "install" :: "--allow-root" :: "--force-latest" :: Nil, bd, "OUTPUT_DIR" -> out.toString) !)
-      if (exitCode != 0) sys.error(s"Bower nonzero exit code: ${exitCode}. Aborting!")
-      (out ***).get
+    // bower
+    bowerExecutable := baseDirectory.value / "node_modules/bower/bin/bower",
+    bowerJsonFile := baseDirectory.value / "bower.json",
+    bowerComponentsDirectory := baseDirectory.value / "bower_components",
+    bower := {
+      if (bowerJsonFile.value.exists) {
+        val exitCode = (Process(bowerExecutable.value.absolutePath :: "install" :: "--allow-root" :: "--force-latest" :: Nil,
+                                baseDirectory.value) !)
+        if (exitCode != 0) sys.error(s"Bower nonzero exit code: ${exitCode}. Aborting!")
+      }
     },
     bower <<= bower.dependsOn(npm),
-    resourceGenerators in Compile <+= bower,
 
     // Grunt
-    gruntExecutable := "node_modules/grunt-cli/bin/grunt",
-
-    gruntClientDirectory <<= baseDirectory apply (bd => bd / "src/main/client"),
-    gruntOutputDirectory <<= (resourceManaged in Compile) apply (out => out / "public"),
-
-    gruntWatchSources <<= gruntClientDirectory map (d => (d ***).get),
-    watchSources <<= (watchSources, gruntWatchSources) map ( (ws, gws) => ws ++ gws),
-
-    grunt <<= (gruntExecutable, baseDirectory, gruntOutputDirectory) map { (ge, bd, out) =>
-      val exitCode = (Process(ge, bd, "OUTPUT_DIR" -> out.toString) !)
+    gruntExecutable := baseDirectory.value / "node_modules/grunt-cli/bin/grunt",
+    gruntOutputDirectory := (resourceManaged in Compile).value / "public",
+    gruntWatchSources := (clientSrcDirectory.value ***).get,
+    watchSources := (watchSources.value ++ gruntWatchSources.value),
+    gruntDefaultTask := "default",
+    grunt := {
+      val out = gruntOutputDirectory.value
+      val exitCode = (Process(gruntExecutable.value.absolutePath :: gruntDefaultTask.value :: Nil, baseDirectory.value,
+                              "OUTPUT_DIR" -> out.toString) !)
       if (exitCode != 0) sys.error(s"Grunt nonzero exit code: ${exitCode}. Aborting!")
       (out ***).get
     },
-    grunt <<= grunt.dependsOn(npm),
     grunt <<= grunt.dependsOn(bower),
     resourceGenerators in Compile <+= grunt
   )
