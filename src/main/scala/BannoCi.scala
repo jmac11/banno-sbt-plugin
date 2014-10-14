377b30a4370309d54ff5d30ba3ea4e4867381946
jj <jj@banno.com>
2014-10-14 13:57:45 -0500

    Add's docker snapshot building to the 'ci' command.

--- src/main/scala/BannoCi.scala
+++ src/main/scala/BannoCi.scala
@@ -1,11 +1,36 @@
 package com.banno
 import sbt._
 import Keys._
+import complete.DefaultParsers._
+import Docker._
+import sbtdocker._
+import sbtdocker.Plugin._
+import sbtdocker.Plugin.DockerKeys._
+
+import scala.util.Try
 
 object BannoCi {
-  val ciTask = TaskKey[Unit]("ci")
+  val ciTask = TaskKey[Unit]("ci-without-docker")
 
   val settings = Seq(
-    ciTask <<= publish.dependsOn((test in Test).dependsOn(clean))
+    commands += ci,
+    ciTask <<= {
+      publish.dependsOn((test in Test).dependsOn(clean))
+    }
   )
+
+  val ci: Command = Command("ci")(_ => (Space ~> "skip-tests").*) { (st, args) =>
+    val extract = Project.extract(st)
+    Project.runTask(ciTask.scopedKey, st)
+
+    val useDocker = extract.getOpt(Docker.packageUsingDocker)
+    if (useDocker.getOrElse(false)) {
+      Try(extract.runTask(Docker.dockerPullLatest, st))
+      extract.runTask(docker, st)
+      extract.runTask(Docker.dockerPush, st)
+      extract.runTask(Docker.dockerPushLatestTag, st)
+    }
+
+    st
+  }
 }
---
+++ src/sbt-test/banno-sbt-plugin/ci-docker/build.sbt
@@ -0,0 +1,7 @@
+import com.banno._
+
+name := "sbt-ci-docker-test"
+
+BannoSettings.settings
+
+Docker.settings
