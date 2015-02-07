4f6f9fb9aa2f293a21a819c08c5a8e0296fa8732
Adam Shannon <adam.shannon@banno.com>
2015-02-07 11:40:26 -0600

    adding a couple more compiler flags that are helpful

--- src/main/scala/BannoCompile.scala
+++ src/main/scala/BannoCompile.scala
@@ -4,7 +4,7 @@ import Keys._
 
 object BannoCompile {
   val settings = Seq(
-    scalacOptions := Seq("-deprecation", "-feature", "-language:postfixOps",
+    scalacOptions := Seq("-deprecation", "-feature", "-language:postfixOps", "-Xlint", "-Xlog-free-terms", "-Xlog-free-types",
                          "-language:implicitConversions", "-language:higherKinds", "-language:existentials", "-language:postfixOps",
                          "-Ywarn-dead-code", "-Ywarn-numeric-widen", "-Ywarn-inaccessible", "-unchecked"),
     scalacOptions in Test += "-language:reflectiveCalls"
