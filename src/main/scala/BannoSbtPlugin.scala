4fca6d20dff38735c0b1d752e885529751ad6367
Luke Amdor <luke.amdor@banno.com>
2015-02-11 10:22:06 -0600

    adding auto-update on start to global settings

--- src/main/scala/BannoSbtPlugin.scala
+++ src/main/scala/BannoSbtPlugin.scala
@@ -2,6 +2,7 @@ package com.banno
 import sbt._
 
 object BannoSbtPlugin extends AutoPlugin {
-  override lazy val buildSettings = BannoSettings.buildSettings
+  override lazy val buildSettings = BannoSettings.buildSettings 
   override lazy val projectSettings = BannoSettings.settings
+  override lazy val globalSettings = BannoSbtPluginAutoUpdating.globalSettings
 }
