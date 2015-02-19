bff032b51a619e28119a57fc249b39aeff8ae7e2
Adam Shannon <adam.shannon@banno.com>
2015-02-19 15:36:13 -0600

    Revert "adding in a timestamp for when this app was compiled"
    This reverts commit b0424e3bc22a11936bf9e532f8b55439de7955eb.

--- src/main/scala/BuildInfoSettings.scala
+++ src/main/scala/BuildInfoSettings.scala
@@ -2,7 +2,6 @@ package com.banno
 import sbt._
 import Keys._
 import sbtbuildinfo.Plugin._
-import java.util.Date
 
 object BuildInfoSettings {
   val settings: Seq[Setting[_]] =
@@ -15,9 +14,6 @@ object BuildInfoSettings {
                                          scalaVersion,
                                          sbtVersion,
                                          bannoDependencies,
-                                         BuildInfoKey.action("buildTime") {
-                                           (new Date()).toString
-                                         },
                                          BuildInfoKey.action("gitSha") {
                                            Process("bash" :: "-c" :: "git rev-parse HEAD || echo None" :: Nil) !!
                                          })
