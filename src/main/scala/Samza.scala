8a5fb4ec33eb8dde12e15d6f102a47a8e650c601
jwinder <jsph.winder@gmail.com>
2015-03-02 14:47:24 -0600

    Ignoring some flags for samza

--- src/main/scala/Samza.scala
+++ src/main/scala/Samza.scala
@@ -6,6 +6,7 @@ import sbtdocker.Plugin.DockerKeys.docker
 
 object Samza {
   val settings = Docker.settings ++ Seq(
+    javaOptions in docker ~= { _.filterNot(opt => opt.contains("Xms") || opt.contains("Xmx")) },
     mainClass in Compile := Some("com.banno.samza.Main"),
     baseImage in docker := "banno/samza-mesos:0.21.1",
     //Mesos native lib binds to LIBPROCESS_IP so the Mesos master can communicate with the Mesos framework, so it needs to be an IP accessible by Mesos master (not 127.0.0.1)
