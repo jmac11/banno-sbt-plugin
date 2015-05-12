9e3f94838ad062ef0a96d406e7d6e18068d4fcdf
Eric Heikes <eric.heikes@banno.com>
2015-05-12 15:28:13 -0500

    Run gulp tasks if gulp is installed and gulpfile.js exists.

--- src/main/scala/Grunt.scala
+++ src/main/scala/Grunt.scala
@@ -12,6 +12,12 @@ object Grunt {
   val bowerComponentsDirectory=  SettingKey[File]("bower-components-directory")
   val bower = TaskKey[Unit]("bower")
 
+  val gulpExecutable = SettingKey[File]("gulp-executable")
+  val gulpOutputDirectory = SettingKey[File]("output-directory")
+  val gulpFile = SettingKey[File]("gulp-file")
+  val gulpDefaultTask = SettingKey[String]("gulp-default-task")
+  val gulp = TaskKey[Seq[File]]("gulp")
+
   val gruntWatchSources = TaskKey[Seq[File]]("grunt-watch-sources")
   val gruntExecutable = SettingKey[File]("grunt-executable")
   val gruntOutputDirectory = SettingKey[File]("output-directory")
@@ -44,6 +50,23 @@ object Grunt {
       }
     },
     bower <<= bower.dependsOn(npm),
+
+    // gulp
+    gulpExecutable := baseDirectory.value / "node_modules/.bin/gulp",
+    gulpOutputDirectory := (resourceManaged in Compile).value / "public",
+    gulpFile := baseDirectory.value / "gulpfile.js",
+    gulpDefaultTask := "default",
+    gulp := {
+      val out = gulpOutputDirectory.value
+      if (gulpExecutable.value.exists && gulpFile.value.exists) {
+        val exitCode = (Process(gulpExecutable.value.absolutePath :: gulpDefaultTask.value :: Nil,
+                                baseDirectory.value, "OUTPUT_DIR" -> out.toString) !)
+        if (exitCode != 0) sys.error(s"Gulp nonzero exit code: ${exitCode}. Aborting!")
+      }
+      (out ***).get
+    },
+    gulp <<= gulp.dependsOn(bower),
+    resourceGenerators in Compile <+= gulp,
 
     // Grunt
     gruntExecutable := baseDirectory.value / "node_modules/grunt-cli/bin/grunt",
